939b2cc7cc841155e05f015140c4278fcef370f7
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-12-17 14:10:38 -0600

    added query to add ad into the adsdao

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -2,6 +2,8 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.Ad;
 import com.mysql.cj.jdbc.Driver;
+import com.codeup.adlister.Config;
+
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -41,8 +43,8 @@ public class MySQLAdsDao implements Ads {
     @Override
     public Long insert(Ad ad) {
         try {
-            String insertQuery = "INSERT INTO ads(user_id, title, description) VALUES (?, ?, ?)";
-            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+//            String insertQuery = "INSERT INTO ads(user_id, title, description) VALUES (1, 'for sale', 'whatever whatever')";
+            PreparedStatement stmt = connection.prepareStatement(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
             stmt.setLong(1, ad.getUserId());
             stmt.setString(2, ad.getTitle());
             stmt.setString(3, ad.getDescription());
@@ -53,6 +55,13 @@ public class MySQLAdsDao implements Ads {
         } catch (SQLException e) {
             throw new RuntimeException("Error creating a new ad.", e);
         }
+    }
+
+    public String createInsertQuery (Ad ad) {
+        return "INSERT INTO ads(user_id, title, description) VALUES "
+            + "(" + ad.getUserId() + ","
+            + "'" + ad.getTitle() + ","
+            + "'" + ad.getDescription() + ")";
     }
 
     private Ad extractAd(ResultSet rs) throws SQLException {
