0a7a72b91c264c99548520ba1e4bb1cf609e0dc2
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-18 15:36:46 -0600

    pseudocode done for the edit user servlet

--- src/main/java/com/codeup/adlister/controllers/EditUserServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditUserServlet.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class EditUserServlet extends HttpServlet {
 //    DOGET
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("/WEB-INF/edit_user.jsp").forward(request, response);
     }
 
 //================================================
@@ -22,6 +22,28 @@ public class EditUserServlet extends HttpServlet {
 
 //    DOPOST
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //Grabbing params from form
+        String new_username = request.getParameter("new_username");
 
+        String new_password = "";
+        //Checking to see if the new passwords entered match....
+        if(request.getParameter("new_password").equals(request.getParameter("confirm_new_password"))){
+            //...and if so, assigning the value to a variable
+            new_password = request.getParameter("new_password");
+        } else {
+            //TODO: alert user the passwords need to match
+        }
+
+        User updated_user = null;
+        //Checking to see if any of the input is null and making the new user object accordingly...
+        if(new_username.isEmpty() && !new_password.isEmpty()){
+            //TODO: Update Password
+        } else if (new_password.isEmpty()){
+            //TODO: Update username
+        } else{
+           //TODO: Inform the user there's no point submitting the page unless they want to change something (and redirect to the edit page)
+        }
+
+        //TODO: Redirect to UPDATED profile page!!!!!!!
     }
 }
