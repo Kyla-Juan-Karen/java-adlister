3964f87d85bb8180f2a262a3403d89826d1d0c2c
Kyla Allen <themightyflorp@kylas-air.attlocal.net>
2019-01-06 18:29:43 -0600

    added the users attribute to the admin page servlet

--- src/main/java/com/codeup/adlister/controllers/AdminPageServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdminPageServlet.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,11 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet (name="AdminPageServlet", urlPatterns = "/admin")
 public class AdminPageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        DaoFactory.getUsersDao().
+        List<User> users = DaoFactory.getUsersDao().allUsers();
+        request.getSession().setAttribute("users", users);
         request.getRequestDispatcher("/WEB-INF/admin_page.jsp");
     }
 
